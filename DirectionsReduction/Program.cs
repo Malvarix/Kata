c4e3f8b7d6119e3b892859dbe1df8cce4583d54b
Malvarix <55942075+Malvarix@users.noreply.github.com>
2020-07-11 10:54:55 +0300

    Add files via upload
    Task
    Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
    
    The Haskell version takes a list of directions with data Direction = North | East | West | South.
    The Clojure version returns nil when the path is reduced to nothing.
    The Rust version takes a slice of enum Direction {NORTH, SOUTH, EAST, WEST}.
    
    For example : { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
    Result : { "WEST" }

---
+++ DirectionsReduction/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace ConsoleApp12_Training1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(string s in dirReduce(new string[] { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" }))
+            {
+                Write("{0} ", s);
+            }
+                                     
+            ReadKey();
+        }
+
+        public static string[] dirReduce(String[] arr)
+        {
+            Dictionary<string, string> oppositeSides = new Dictionary<string, string>()
+            {
+                {"NORTH","SOUTH"},
+                {"SOUTH","NORTH"},
+                {"WEST","EAST" },
+                {"EAST","WEST" }
+            };
+            List<string> theBestDir = new List<string>();
+            foreach(string s in arr)
+            {
+                if (theBestDir.LastOrDefault() == oppositeSides[s])
+                    theBestDir.RemoveAt(theBestDir.Count - 1);
+                else
+                    theBestDir.Add(s);
+            }
+            return theBestDir.ToArray();
+        }
+    }
+}
