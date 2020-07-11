bbe35c64567de9995a6ad43591b5b664c74c221a
Malvarix <55942075+Malvarix@users.noreply.github.com>
2020-07-11 23:16:44 +0300

    Add files via upload
    Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
    
    Rules for a smiling face:
    
    Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
    A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
    Every smiling face must have a smiling mouth that should be marked with either ) or D
    No additional characters are allowed except for those mentioned.
    
    Valid smiley face examples: :) :D ;-D :~)
    Invalid smiley faces: ;( :> :} :]
    
    Examples : 
    countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
    countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
    countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;

---
+++ Count the smiley faces!/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp12_Training1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {      
+		
+            ReadKey();
+        }
+        public static int CountSmileys(string[] smileys)
+        {
+            int result = 0;
+            foreach(string s in smileys)
+            {
+                if (Regex.IsMatch(s, @"^(:|;)(?:-|~)?(\)$|D)$"))
+                    result++;
+            }
+            return result;
+        }
+		
+		public static int CountSmileys2(string[] smileys) // second version better than first, if you use know how to use linq
+        {
+			return smileys.Count(s => Regex.IsMatch(s, @"^(:|;)(?:-|~)?(\)$|D)$"));
+        }
+    }
+}
