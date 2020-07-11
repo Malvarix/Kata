c5f05de9d9b68dd34dddec9c016beafa13a6692b
Malvarix <55942075+Malvarix@users.noreply.github.com>
2020-07-11 13:19:20 +0300

    Add files via upload
    ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
    
    Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".

---
+++ ROT13/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace ConsoleApp12_Training1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {      
+            WriteLine(Rot13("tEst"));
+            ReadKey();
+        }
+        public static string Rot13(string message)
+        {
+            char[] array = message.ToCharArray();
+            for(int i = 0; i < message.Length; i++)
+            {
+                int index = array[i];
+                if(index >= 'a' && index <= 'z')
+                {
+                    if (index > 'm')
+                        index -= 13;
+                    else
+                        index += 13;
+                }
+                else if(index >= 'A' && index <= 'Z')
+                {
+                    if (index > 'M')
+                        index -= 13;
+                    else
+                        index += 13;
+                }
+                array[i] = (char)index;
+            }
+            return string.Concat(array);
+        }
+    }
+}
