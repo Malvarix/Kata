b17ae8b908cfd05751e39f09ae1b7d8bc505a0a9
Malvarix <55942075+Malvarix@users.noreply.github.com>
2020-07-12 18:58:06 +0300

    Add files via upload
    A stream of data is received and needs to be reversed.
    
    Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
    
    11111111  00000000  00001111  10101010
     (byte1)   (byte2)   (byte3)   (byte4)
    should become:
    
    10101010  00001111  00000000  11111111
     (byte4)   (byte3)   (byte2)   (byte1)
    The total number of bits will always be a multiple of 8.
    
    The data is given in an array as such:
    
    [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]

---
+++ Data Reverse/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp12_Training1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {      
+		
+        }
+        public static int[] DataReverse2(int[] data) // DataReverse = DataReverse2, but it's smarter than DataReverse
+        {
+            int[] bits = data;
+            for(int i = 0; i < data.Length; i += 8)
+            {
+                Array.Reverse(bits, i, 8);
+            }
+            Array.Reverse(bits);
+            return bits;
+        }
+        public static int[] DataReverse(int[] data)
+        {
+            string results = string.Join("", data);
+            int temp1 = 0;
+            string temp2 = "";
+            List<string> temp3 = new List<string>();
+            for(int i = 0; i < results.Length; i++)
+            {
+                if(temp1 != 7)
+                {
+                    temp2 += results[i];
+                    temp1++;
+                }
+                else if(temp1 == 7)
+                {
+                    temp2 += results[i];
+                    temp3.Add(temp2);
+                    temp2 = "";
+                    temp1 = 0;
+                }
+            }
+            temp3.Reverse();
+            List<int> res = new List<int>();
+            foreach(string s in temp3)
+            {
+                foreach(char c in s)
+                {
+                    res.Add(Int32.Parse(c.ToString()));
+                }
+            }
+            return res.ToArray(); 
+        }
+    }
+}
